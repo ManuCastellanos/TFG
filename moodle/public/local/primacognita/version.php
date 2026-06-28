69a4a51ae8806fa8c2e83bb6040a555e5db85578
Manu Castellanos <manuiniestacastellanos@gmail.com>
2026-06-28 20:32:15 +0200

    Actualizamos versión moodle

--- moodle/public/local/primacognita/version.php
+++ moodle/public/local/primacognita/version.php
@@ -1,6 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_primacognita';
-$plugin->version   = 2025010108;
+$plugin->version   = 2025010109;
 $plugin->requires  = 2025100600;
 $plugin->maturity  = MATURITY_STABLE;
