9d28a2901d3dd41d231ed170e6de1ce7f6f7f4cd
Manu Castellanos <manuiniestacastellanos@gmail.com>
2026-07-06 10:57:37 +0200

    Arreglamos el login para que el usuario no se pueda registrar

--- primacognita-web/src/features/login/pages/LoginPage.tsx
+++ primacognita-web/src/features/login/pages/LoginPage.tsx
@@ -2,7 +2,6 @@ import { useNavigate } from '@tanstack/react-router';
 import { Card } from '@/components/ui/card/Card';
 import { AuthPage } from '@/components/layout/authPage/AuthPage';
 import { Text } from '@/components/ui/text/Text';
-import { Button } from '@/components/ui/button/Button';
 import { useLogin } from '../hooks/useLogin';
 import { LoginForm } from '../components/LoginForm';
 import type { LoginCredentials } from '../types/login.types';
@@ -31,14 +30,7 @@ function LoginPage() {
 
         <div className="my-6 h-px bg-(--border)" />
 
-        <section className="space-y-2">
-          <Text className="text-xl font-semibold text-(--fg)">Registrarse como usuario</Text>
-          <Text className="max-w-xs text-sm text-(--muted)">Crea tu cuenta para acceder al campus virtual.</Text>
-
-          <Button type="button" variant="outline" className="mt-3" onClick={() => navigate({ to: '/signup' })}>
-            Crear nueva cuenta
-          </Button>
-        </section>
+        <Text className="text-sm text-(--muted)">Contacta con el administrador para tus credenciales</Text>
       </Card>
     </AuthPage>
   );
